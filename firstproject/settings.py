78b02394ce192140ccc116286ef2d9e143b1839c
Mohd Wasim <wasim.ahmad0661@gmail.com>
2025-11-29 11:27:58 +0530

    Refactor static file settings in settings.py
    Removed STATICFILES_DIRS and updated STATIC_ROOT.

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -118,10 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
